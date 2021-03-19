72a9313074e73e7fd67177f0dbb86ab9ae051a8c
18080398 <ielc18.ddelacruzm@itesco.edu.mx>
2021-03-18 19:12:44 -0600

    Revert "18080398"
    This reverts commit 6a087f7ff666532725e9b5b5c3d1379bcc3afa41.

--- Ex_3_4.py
+++
@@ -1,9 +0,0 @@
-
-contraseñacorrecta="iloveyou123"
-contraseña=input("ingrese la contraseña")
-while (contraseña!=contraseñacorrecta):
-  print("la contraseña es incorrecta")
-  string= input("intentelo de nuevo")
-  
-print("contraseña correcta")
-print("bienvenido")
--- Ex_3_5.py
+++
@@ -1,9 +0,0 @@
-i=float(input('ingrese un numero'))
-if(i<0):
-    print("el numero es negativo")
-    if (i>0):
-        print("el numero es positivo")
-        if (i==0):
-            print("le numero es cero")
-            
-            
